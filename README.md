e7dba2d5192edf1d4e25cf6545b6177bdc71ea57
ruhuang <ruhuang2001@gmail.com>
2022-03-06 17:25:33 +0800

    Update README and add some code in VGA Text Mode

--- README.md
+++ README.md
@@ -3,4 +3,12 @@ Thanks to this repository : [phil-opp/blog_os](https://github.com/phil-opp/blog_
 
 I just follow it and code.
 
+## Resources
+To be honest, I am unfamiliar with Rust(Just be interest in it). Here are some resources that help me to read the code.
 
+**YOUTUBE**
+- [Rust Crash Course | Rustlang  -- Traversy Media](https://www.youtube.com/watch?v=zF34dRivLOw)
+- [Rust for Beginners -- Microsoft Developer](https://www.youtube.com/playlist?list=PLlrxD0HtieHjbTjrchBwOVks_sr8EVW1x)
+
+**BOOK**
+- [The Rust Programming Language](https://doc.rust-lang.org/stable/book/)
---
+++ src/vga_buffer.rs
@@ -0,0 +1,66 @@
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    asill_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+
+pub struct Writer {
+    column_positon: usize,
+    color_code: ColorCode,
+    // vaild when running
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            
+        }
+    }
+}
