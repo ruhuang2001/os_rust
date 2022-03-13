b6788118fb56b0dcdcab33c6cbd3aa9874a891fd
ruhuang <ruhuang2001@gmail.com>
2022-03-13 18:10:21 +0800

    read Testing: Integration Tests section

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    println!("Running {} tests ", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");    
+}
+
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}",_info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[Failed]\n");
+    serial_println!("Error: {}",_info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -27,6 +61,25 @@ pub extern "C" fn _start() -> ! {
     // write!(vga_buffer::WRITER.lock(), ",some numbers: {} {}", 42, 12.42).unwrap();
 
     println!("Hello World{}", "!");
+    #[cfg(test)]
+    test_main();
     loop{}
 
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
