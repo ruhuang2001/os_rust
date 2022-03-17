da948fa508599a228c12804ff21833594cbd3dac
ruhuang2001 <ruhuang2001@gmail.com>
2022-03-17 17:56:54 +0800

    start Double Faults

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,17 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     os_rust::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // };
+    
+    fn stack_overflow() {
+        stack_overflow();
+    }
 
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
