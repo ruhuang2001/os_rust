bd7a711111a4c6de452eac3be690250cd0897074
ruhuang2001 <ruhuang2001@gmail.com>
2022-04-05 20:29:43 +0800

    completed Paging Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-use core::{panic::PanicInfo, ptr};
+
+use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use x86_64::structures::paging::PageTable;
-mod vga_buffer;
-mod serial;
+use os_rust::println;
 
 entry_point!(kernel_main);
 
 #[no_mangle]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    // TODO
-    use os_rust::memory::active_level_4_table;
-    use x86_64::{structures::paging::Translate, VirtAddr};
-    use os_rust::memory::translate_addr;
-    use os_rust::memory;
+
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use os_rust::memory::{self, BootInfoFrameAllocator};
 
 
     println!("Hello World{}", "!");
@@ -25,40 +22,49 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    
+    
+    // let addresses = [
+    //     0xb8000,
+    //     0x201008,
+    //     0x0100_0020_1a10,
+    //     boot_info.physical_memory_offset,
+    // ];
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
-    let l4_table = unsafe {
-        active_level_4_table(phys_mem_offset)
-    };  
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
+    // let l4_table = unsafe {
+    //     active_level_4_table(phys_mem_offset)
+    // };  
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe {
-                &*ptr
-            };
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe {
+    //             &*ptr
+    //         };
 
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }    
-            }
-        }
-    }
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("  L3 Entry {}: {:?}", i, entry);
+    //             }    
+    //         }
+    //     }
+    // }
     #[cfg(test)]
     test_main();
 
@@ -70,52 +76,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     os_rust::hlt_loop();
 }
 
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    os_rust::hlt_loop();
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    println!("Running {} tests ", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os_rust::test_panic_handler(info)
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");    
-}
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    os_rust::test_panic_handler(_info)
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}",_info);
-    os_rust::hlt_loop();
 }
