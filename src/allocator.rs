71dc20abc92d104250d5e2f06b5c51882ff1b1a1
ruhuang2001 <ruhuang2001@gmail.com>
2022-04-13 17:37:51 +0800

    start Discussion section

--- src/allocator.rs
+++ src/allocator.rs
@@ -7,14 +7,15 @@ use x86_64::{
     VirtAddr,
 };
 use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
 use bump::BumpAllocator;
 
 pub mod bump;
 pub mod linked_list;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
-
+static ALLOCATOR: Locked<LinkedListAllocator> = 
+    Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
